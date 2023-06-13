78c32ba53105ff7fcec7a03e35f1833b6995dc82
Linda Mei <85714837+ilindaa@users.noreply.github.com>
2023-06-12 23:48:28 -0400

    Added my 01-03 solutions (WIP: 04)

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const arr = string.split("");
+    const revArr = arr.reverse();
+    let str = revArr.join("");
+    return str;
+    /*
+    const arr = string.split("");
+    const revArr = arr.reverse();
+    let str = "";
+    for(let i = 0; i < revArr.length; i++) {
+        str += revArr[i];
+    }
+    return str;
+    */
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, removedElement) {
+    let a = arguments[0];
+    let remArr = [];
+    for(let i = 0; i < a.length; i++) {
+        if(a[i] != removedElement) {
+            remArr.push(a[i]);
+        }
+    }
+    return remArr;
 };
 
 // Do not edit below this line
