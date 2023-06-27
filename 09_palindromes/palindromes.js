af2e444ecdb25be4cb44f7114ee15fbfdc55c7d8
Linda Mei <85714837+ilindaa@users.noreply.github.com>
2023-06-27 16:37:40 -0400

    Added my 09-10 solutions

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // what i could do is cut the punctuation out in the string
+    // so like commas, periods, exclamation marks, spaces
+    // save that into a new str
+    // also set the new str to all lowercase so that case doesn't matter
+    const lowercaseStr = str.toLowerCase();
+    let arr = lowercaseStr.split(' ').join('').split(''); // splits on spaces, joins, and then splits into letters
 
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i].includes(",") || arr[i].includes(".") || arr[i].includes("!")) { // removes the punctuation and spaces by splicing 1 element from the index
+            arr.splice(i, 1);
+        }
+    }
+
+    let forward = arr.join('');
+    let reverse = arr.reverse().join('');
+
+    if(forward === reverse) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
+    if (position < 0) return "OOPS";
+    if (typeof position == "string" ) {
+        position = parseInt(position);
+    }
 
+    let arr = [0, 1];
+    
+    // simplified to 1 for loop (instead of 2)
+    for(let i = 0; i < position; i++) {
+        let add = arr[i] + arr[i+1];
+        arr.push(add);
+    }
+    
+    return arr[position];
 };
 
 // Do not edit below this line
